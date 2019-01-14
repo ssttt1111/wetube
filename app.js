9f093993375ec14dd34f48f35c5c991700e202d3
Suwan Choi <ssttt1111@gmail.com>
2019-01-14 21:15:27 +0900

    #2.13 Installing Pug

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(helmet()); // Security
+app.use(morgan("dev")); // Logging
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
