1f3238b60d840a7d85c2d231ba4c2363e20dbe4e
Suwan Choi <ssttt1111@gmail.com>
2019-01-28 19:40:18 +0900

    #6.4 Sessions on Express

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import session from "express-session";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
@@ -12,7 +13,7 @@ import globalRouter from "./routers/globalRouter";
 
 import "./passport";
 
-const app = express();
+const app = express(process.env.COOKIE_SECRET);
 
 app.use(helmet()); // Security
 app.set("view engine", "pug");
@@ -22,6 +23,13 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev")); // Logging
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
