135398bc09aae6c8a6f4c64550b840a931c5b912
Suwan Choi <ssttt1111@gmail.com>
2019-02-04 05:14:49 +0900

    #6.12 Facebook Log In part Three

--- passport.js
+++ passport.js
@@ -26,7 +26,7 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `https://df3c4d55.ngrok.io${routes.facebookCallback}`,
+      callbackURL: `https://e04fd18b.ngrok.io${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"],
       scope: ["public_profile", "email"]
     },
