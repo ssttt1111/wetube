0f414674607d7f591b7324f66565727cbaab2829
Suwan Choi <ssttt1111@gmail.com>
2019-02-12 04:56:35 +0900

    #10.0 API Registering a View part One

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,15 +3,29 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event.data);
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
   videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
 };
 
 const getVideo = async () => {
@@ -23,7 +37,7 @@ const getVideo = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
-    recordBtn.innerHTML = "Stop Recording";
+    recordBtn.innerHTML = "Stop recording";
     streamObject = stream;
     startRecording();
   } catch (error) {
@@ -35,7 +49,6 @@ const getVideo = async () => {
 
 function init() {
   recordBtn.addEventListener("click", getVideo);
-  recordBtn.onclick = getVideo;
 }
 
 if (recorderContainer) {
